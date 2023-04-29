0d507456a8123aaf2b2259fbe91f0713df6cdb0b
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-04-29 16:07:43 +0100

    002

---
+++ packages/bahaedd/quickmetrics-laravel/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bahaedd/quickmetrics',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a635c48bd7932b7abf6c83087fd7c71500e805e0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bahaedd/quickmetrics' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a635c48bd7932b7abf6c83087fd7c71500e805e0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
